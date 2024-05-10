5a0225d5ddc54fbf08124a45ae2909d3b1d8fae8
fyni12 <dllamc01@estudiantes.unileon.es>
2024-05-10 20:41:54 +0200

    cambio de estos dos archivos

--- NSProtocol.c
+++ NSProtocol.c
@@ -11,7 +11,8 @@
 // Invalid: all other combinations.
 // ej3: Pulsado arriba y abajo => esta combinacion no es valida
 // por lo que debe sustituirse por centrado
-void NSProtocolDPadButtonsToByte(int up, int down, int left, int right, uint8_t* byte) {
+void NSProtocolDPadButtonsToByte(int up, int down, int left, int right, uint8_t *byte)
+{
 
   // Hay que dar a la variable byte el valor correcto en funcion de que botones estan pulsados.
   // Por ej, si solo esta pulsado el boton de arriba, hariamos:
@@ -22,81 +23,104 @@ void NSProtocolDPadButtonsToByte(int up, int down, int left, int right, uint8_t*
 
   // TO DO
 
-  if(up && !down){
+  if (up && !down)
+  {
 
-    if(left){
-
-    }else if(right){
-       *byte=NSGAMEPAD_DPAD_UP;
-
-    }else{
-      *byte=NSGAMEPAD_DPAD_UP;
+    if (left)
+    {
+      *byte = NSGAMEPAD_DPAD_UP_LEFT;
     }
+    else if (right)
+    {
+      *byte = NSGAMEPAD_DPAD_UP_RIGHT;
+    }
+    else
+    {
+      *byte = NSGAMEPAD_DPAD_UP;
+    }
+  }
+  else if (down && !up)
+  {
 
+    if (left)
+    {
+      *byte = NSGAMEPAD_DPAD_DOWN_LEFT;
+    }
+    else if (right)
+    {
+      *byte = NSGAMEPAD_DPAD_DOWN_RIGHT;
+    }
+    else
+    {
+      *byte = NSGAMEPAD_DPAD_DOWN;
+    }
+  }
+  else if (right && !left)
+  {
+    *byte = NSGAMEPAD_DPAD_RIGHT;
+  }
+  else if (left && !right)
+  {
+    *byte = NSGAMEPAD_DPAD_LEFT;
+  }
+  else
+  {
+    *byte = NSGAMEPAD_DPAD_CENTERED;
+  }
+}
 
+// Funcion que rellena los campos actionButtons y menuButtos de nsGamepad.
+// El primer byte representa el estado de los primeros 8 botones (los de accion)
+// del tipo tNSButtons.
+// Estos botones ya se definieron en este mismo orden en el tipo enumerado tNSButtons de NSTypes.h:
+//  Y B A X L R ZL ZR
+// Cada bit representa si el boton correspondiente esta pulsado (1) o no lo esta (0).
+// El peso del bit coincide con la posicion del boton en el enumerado (el boton Y va en el 0, el B en el 1...).
+// Ejemplo: Si solo esta pulsado el boton B, es decir, el de indice 1, y el resto no lo estan,
+// la secuenca seria: 00000010
+// El segundo byte, menuButtons, corresponde al estado de los botones:
+// MINUS PLUS LEFT_STICK RIGHT_STICK HOME CAPTURE
+// (seis botones, los dos bits restantes los dejamos a 0)
+void NSProtocolReportDataSetButtons(tNSGamepad nsGamepad, tNSGamepadReportData *NSGamepadReportData)
+{
 
-  }else if(down && !up ){
+  // Primero los botones de accion, en el mismo orden que el enumerado:
+  // Y B A X L R ZL ZR
 
-  }else if(right && !left){
+  // Inicializamos a 0
+  NSGamepadReportData->actionButtons = 0x00;
+  int i;
+  // Comenzamos por el primero de ellos, el Y (ver tNSButtons de NSTypes.h)
+  for(i=0; i<ZR; i++){
 
-  }else if (left && !right){
-
-  }else{
-      *byte=NSGAMEPAD_DPAD_CENTERED;
+    NSGamepadReportData->actionButtons = NSGamepadReportData->actionButtons | (uint8_t)nsGamepad.buttonsPressed[i]<<i;
   }
 
 
-
-
-
-}
-
-// Funcion que devueve en dos bytes (16 bits) un valor que representa el estado de los primeros 14 botones
-// del tipo tNSButtons.
-// Segun el protocol NS, el estado de los siguientes 14 botones se envia como un entero de 16 bits
-// en el que cada bit representa si el boton correspondiente esta pulsado (1) o no lo esta (0).
-// Los dos bits restantes (hasta 16) van a 0:
-//
-//  Y,
-//  B,
-//  A,
-//  X,
-//  LEFT_TRIGGER,
-//  RIGHT_TRIGGER,
-//  LEFT_THROTTLE,
-//  RIGHT_THROTTLE,
-//  MINUS,
-//  PLUS,
-//  LEFT_STICK,
-//  RIGHT_STICK,
-//  HOME,
-//  CAPTURE
-
-// ... estos botones ya se definieron en este mismo orden en el tipo enumerado tNSButtons de NSTypes.h.
-// El peso del bit coincide con la posicion del boton en el enumerado (el boton Y va en el 0, el B en el 1...).
-// Ejemplo: Si solo esta pulsado el boton B, es decir, el de indice 1, y el resto no lo estan,
- // la secuenca seria: 00000000 00000010
-void NSProtocolButtonsToBytes (tNSGamepad nsGamepad,  uint16_t* twoBytes) {
-
-  // Inicializamos *twoBytes a 0
-  *twoBytes = 0;
-
-  // Comenzamos por el primero de ellos, el Y (ver tNSButtons de NSTypes.h)
-  *twoBytes = *twoBytes | (uint16_t) nsGamepad.buttonsPressed[Y];
-
-  // Siguiente: B
-  *twoBytes = *twoBytes | (uint16_t) nsGamepad.buttonsPressed[B] << 1;
-
-  // TO DO: Hacer el resto! 
+  // TO DO: Hacer el resto!
   // (Requiere uso de mascaras / desplazamientos logicos)
   // ¿Se puede generalizar usando un bucle?
 
-  int i;
-  for (i = 0; i < UP; i++) {
-       *twoBytes = *twoBytes | (uint16_t) nsGamepad.buttonsPressed[i] << i;
+  // Despues, van los botones de menu, en el mismo orden que el enumerado:
+  //  MINUS PLUS LEFT_STICK RIGHT_STICK HOME CAPTURE
+  // (Los bits de mas peso los dejariamos a cero, parece que depeden del fabricante, para botones extra)
+  // Inicializamos a 0
 
+  NSGamepadReportData->menuButtons = 0x00;
+  for(i=MINUS; i<ZR; i++){
+
+    NSGamepadReportData->actionButtons = NSGamepadReportData->actionButtons | (uint8_t)nsGamepad.buttonsPressed[i]<<i;
   }
-  //!ns si está bien 
+
+  // TO DO: Hacerlos todos, igual que hicimos los de accion
+
+
+  //! ASUMO QUE HAY QUE PASAR TAMBIEN LOS DPADS Y POSTERIORMENTE LOS  JOYSTICKS
+
+
+  NSGamepadReportData->dPad= 0x00;
+  //LE PASA EL ESTADO DE LOS BOTONES DEL DPAD, Y LUEGO LE PASA UN PUNTERO A DONDE TIENE QUE ESCRIBIR
+  NSProtocolDPadButtonsToByte(nsGamepad.buttonsPressed[UP] ,nsGamepad.buttonsPressed[DOWN] , nsGamepad.buttonsPressed[LEFT], nsGamepad.buttonsPressed[RIGHT], NSGamepadReportData->dPad);  
 
 
 
@@ -104,43 +128,37 @@ void NSProtocolButtonsToBytes (tNSGamepad nsGamepad,  uint16_t* twoBytes) {
 }
 
 // Valores por defecto del report que se envia a la Switch (sacado de documentacion "oficial")
-void NSProtocolReportDataSetDefaultValues (tNSGamepadReportData*  NSGamepadReportData) {
+void NSProtocolReportDataSetDefaultValues(tNSGamepadReportData *NSGamepadReportData)
+{
 
-  NSGamepadReportData->buttons = 0x00;
+  NSGamepadReportData->actionButtons = 0x00;
+  NSGamepadReportData->menuButtons = 0x00;
   NSGamepadReportData->dPad = NSGAMEPAD_DPAD_CENTERED;
   NSGamepadReportData->leftXAxis = 0x80;
   NSGamepadReportData->leftYAxis = 0x80;
-  NSGamepadReportData->rightXAxis = 0x80; 
+  NSGamepadReportData->rightXAxis = 0x80;
   NSGamepadReportData->rightYAxis = 0x80;
   NSGamepadReportData->filler = 0x00;
-
 }
 
-void NSProtocolSerializeNSGamepadData (tNSGamepad nsGamepad, uint8_t buffer [NS_GAMEPAD_REPORT_SIZE]) {
+void NSProtocolSerializeNSGamepadData(tNSGamepad nsGamepad, uint8_t buffer[NS_GAMEPAD_REPORT_SIZE])
+{
 
-  tNSGamepadReportData  NSGamepadReportData;
- 
+  tNSGamepadReportData NSGamepadReportData;
+
   // Cargamos los valores por defecto del report
   // (este paso nos lo podemos saltar cuando tengamos hecho el siguiente!)
-  NSProtocolReportDataSetDefaultValues (&NSGamepadReportData);  //!esta en la clase nstypes
+  NSProtocolReportDataSetDefaultValues(&NSGamepadReportData);
 
-  // Pasamos de nsGamepad (tipo tNSGamepad) a tNSGamepadReportData
-  NSProtocolButtonsToBytes (nsGamepad, &NSGamepadReportData.buttons);
+  // Rellenamos NSGamepadReportData (tNSGamepadReportData) partiendo de
+  // los campos de nsGamepad (tipo tNSGamepad).
+  // Botones de accion y de menu:
+  NSProtocolReportDataSetButtons(nsGamepad, &NSGamepadReportData);
   // TO DO: Hacer el resto de campos de NSGamepadReportData!
-
-
-
 
   // Finalmente, serializamos tHID_NSGamepadReportData (metemos los datos en el buffer).
   // TO DO: NO usar memcpy para la estructura completa (puede fallar si la estructura contiene "padding"),
   // hacerlo a mano: un memcpy para cada campo de la estructura, uno a uno, en el mismo orden en que
   // estan definidos en ella (que es el orden que establece el protocolo de la Nintendo Switch).
-  //*memcpy(buffer, &NSGamepadReportData, NS_GAMEPAD_REPORT_SIZE);
-
-  
-
+  memcpy(buffer, &NSGamepadReportData, NS_GAMEPAD_REPORT_SIZE);
 }
-
-
-
-
